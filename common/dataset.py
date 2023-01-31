49174801b49acb7ec83ea826fef62fd01d2ebe4a
Majid <zarharan.majid@gmail.com>
2023-01-31 14:55:56 +0330

    Add dataset and preprocessor class to read and clean data. Add dataset files.

---
+++ common/dataset.py
@@ -0,0 +1,110 @@
+import numpy as np
+from pathlib import Path
+import pandas as pd
+from common.preprocessor import Preprocessor
+
+
+class PubHealthDataset():
+  '''
+  The PubHealthDataset object contains different types of dataset for example: datafram, list of tokens, or list of cleaned text, etc.
+  :param train_path: The path of train set
+  :type train_path: str
+  :param val_path: The path of validation set
+  :type val_path: str
+  :param test_path: The path of test set
+  :type test_path: str
+
+  :ivar df_orginal_trainset: a pandas datafram which includes original train set
+  :vartype df_orginal_trainset: datafram
+  :ivar df_orginal_testset: a pandas datafram which includes original test set
+  :vartype df_orginal_testset: datafram
+  :ivar df_orginal_valset: a pandas datafram which includes original validation set
+  :vartype df_orginal_valset: datafram
+  :ivar label_space: list of all labels in the dataset
+  :vartype label_space: list
+  :ivar pre_processor: an object for different pre processing tasks
+  :vartype pre_processor: object
+  :ivar all_available_sets: list of different section of the dataset (train, val, test)
+  :vartype all_available_sets: list
+  '''
+
+  def __init__(self, train_path, val_path="", test_path= ""):    
+
+    self.df_orginal_trainset= self.read_dataset(train_path, 'train')
+    self.df_orginal_testset= None
+    self.df_orginal_valset= None
+
+    if val_path is not None and len(val_path)>0:
+      self.df_orginal_valset = self.read_dataset(val_path, 'validation')
+    
+    if test_path is not None and len(test_path)>0:
+      self.df_orginal_testset = self.read_dataset(test_path, 'test')
+
+    self.label_space= ['unproven','true','false','mixture']
+    self.pre_processor= Preprocessor()
+    self.all_available_sets= ['train', 'val', 'test']
+
+
+  def read_dataset(self, data_path, set_title):
+    ''' This function gets the path and title of dataset (tsv) and read it into dataframe.
+    
+    :param data_path: The path of tsv file
+    :type text: str
+    :param set_title: The title of target set. Acceptable values are train, val, and test.
+    :type set_title: str
+
+    :returns: pandas dataframe
+    :rtype: object
+    '''
+
+    assert set_title in ['train', 'val', 'test'], f"Acceptable values for set_title are {all_available_sets}."
+    path = Path(data_path)
+    assert path.is_file(), f"Please enter the correct path to the {set_title} set."
+
+    temp_set= pd.read_table(data_path)
+    if set_title== "train":
+      self.df_orginal_trainset= temp_set
+    elif set_title== "val":
+      self.df_orginal_valset= temp_set
+    else:
+      self.df_orginal_testset= temp_set
+
+    return temp_set
+
+
+  def get_k_rand_per_class(self, k= 2, target_set= 'train'):
+    ''' This function selects k instances per label randomly and cleans them by using pre_processor object.
+    
+    :param k: The number of samples per class.
+    :type text: int
+    :param target_set: The target set to select from. Acceptable values are train, val, and test.
+    :type text: str
+
+    :returns: List of Cleaned examples
+    :rtype: list
+    '''
+
+    assert target_set in ['train', 'val', 'test'], f"Acceptable values for target_set are {all_available_sets}."
+
+    self.k_rand_clean_examples= []
+    fn = lambda obj: obj.loc[np.random.choice(obj.index, k, False),:]
+
+    if target_set== "train":
+      assert self.df_orginal_trainset is not None, "Please read the train set at first!"
+      temp_df= self.df_orginal_trainset.loc[self.df_orginal_trainset['label'].isin(self.label_space)]
+    elif target_set== "val":
+      assert self.df_orginal_valset is not None, "Please read the validation set at first!"
+      temp_df= self.df_orginal_valset.loc[self.df_orginal_valset['label'].isin(self.label_space)]
+    else:
+      assert self.df_orginal_testset is not None, "Please read the test set at first!"
+      temp_df= self.df_orginal_testset.loc[self.df_orginal_testset['label'].isin(self.label_space)]
+
+    temp_df= temp_df.groupby('label', as_index=False).apply(fn)
+    temp_df= temp_df.sample(frac=1)
+
+    for index, row in temp_df.iterrows():
+      self.k_rand_clean_examples.append({"claim":self.pre_processor.clean_text(row['claim'])
+          ,"main_text":self.pre_processor.clean_text(row['main_text']),"label":row['label']
+          , "explanation":self.pre_processor.clean_text(row['explanation'])})
+
+    return self.k_rand_clean_examples
---
+++ common/preprocessor.py
@@ -0,0 +1,104 @@
+import re
+from bs4 import BeautifulSoup
+
+
+class Preprocessor():
+  '''
+  The Preprocessor object is responsible for cleaning the text of instances.
+
+  :ivar emoji_pattern: emoji patterns to omit from the text
+  :vartype emoji_pattern: regex
+  :ivar pattern_list: list of patterns to omit from the text
+  :vartype pattern_list: list
+  '''
+
+  def __init__(self):
+
+    self.emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642"
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+        "]+", flags=re.UNICODE)
+
+    self.pattern_list = ["\\u200c|\\u200d|\\u200e|\\u200b|\\u2067|\\u2069"]
+
+
+  def clean_text(self, text):
+    '''this function gets a text and after removing HTML tags, URLs, and specific patterns returns clean text
+
+    :param text: The target text
+    :type text: str
+
+    :returns: Cleaned text
+    :rtype: str
+    '''
+
+    # remove html tags
+    text= self.__remove_html_tags_url(text)
+
+    for pattern in self.pattern_list:
+      text = re.sub(pattern, " ", text)
+
+
+    text = self.__remove_emoji(text)
+    # remove hashtags
+    text = self.__remove_hashtags(text)
+
+    return text
+  
+
+  def __remove_emoji(self, text):
+    '''this function gets a text and removes emoji icons from it
+
+    :param text: The target text
+    :type text: str
+
+    :returns: Cleaned text
+    :rtype: str
+    '''    
+    return self.emoji_pattern.sub(r'', text)    
+
+
+  def __remove_html_tags_url(self, text):
+    '''this function gets a text and removes html tags and Urls
+
+    :param text: The target text
+    :type text: str
+
+    :returns: Cleaned text
+    :rtype: str
+    '''    
+    
+    text= BeautifulSoup(text, features="html.parser").get_text()
+    text= re.sub(r"http\S+", "", text)
+
+    return text
+
+
+  def __remove_hashtags(self, text):
+    '''this function gets a text and removes hashtags
+
+    :param text: The target text
+    :type text: str
+
+    :returns: Cleaned text
+    :rtype: str
+    '''    
+    clean_tokens = [word.strip("#").replace('_', ' ') if word.startswith("#") else word for word in text.split()]
+    clean_tokens = ' '.join(clean_tokens)
+    return clean_tokens
