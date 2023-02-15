91934280e70cd6c76a556325e16da1ace75606c6
Majid <zarharan.majid@gmail.com>
2023-02-15 23:17:31 +0330

    FEATURE: calculate the metrics to evaluate the generated explanation for all files in a directory

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -2,6 +2,7 @@ from common.utils import *
 import argparse
 import pandas as pd
 from pathlib import Path
+import glob
 
 
 def report_metrics(file_path, pred_col_title, target_col_title, metrics_obj):
@@ -9,13 +10,12 @@ def report_metrics(file_path, pred_col_title, target_col_title, metrics_obj):
     path = Path(file_path)
     assert path.is_file(), f"Please enter a correct path to a csv file."
     target_file_df= pd.read_csv(file_path)
-
-    print(target_file_df[pred_col_title].tolist())
-    print(target_file_df[target_col_title].tolist())
     
     metrics_obj.pred_list = target_file_df[pred_col_title].tolist()
     metrics_obj.target_list = target_file_df[target_col_title].tolist()
 
+    print("-"*50)
+    print(f"Calculating metrics for {file_path}:")
     print(metrics_obj.get_all_metrics())
 
 
@@ -28,10 +28,10 @@ def main():
     parser.add_argument("-target_col_title", "--target_col_title", help = "The title of target (ground truth) column to report metrics regarding it"
         , default='explanation', type= str)
     parser.add_argument("-type", "--type"
-        , help = "Report metrics for a single file or all files in a folder", default="folder"
-        , choices=['file', 'folder'], type= str)
+        , help = "Report metrics for a single file or all files in a directory", default="directory"
+        , choices=['file', 'directory'], type= str)
     parser.add_argument("-target_path", "--target_path"
-        , help = "The path to the file or folder to report metrics for them", default="data/pubhealth/prompts/"
+        , help = "The path to the file or directory to report metrics for them", default="data/pubhealth/prompts/"
         , type= str)
     parser.add_argument("-bertscore_model", "--bertscore_model"
         , help = " A name or a model path used to load transformers pretrained model to calculate the Bert score"
@@ -46,9 +46,13 @@ def main():
     if args.type == "file":
         report_metrics(args.target_path, args.pred_col_title, args.target_col_title, nle_metrics)
     else:
-        # ToDo: complete this section later
-        pass
 
+        # csv files in the path
+        files = glob.glob(args.target_path + "/*.csv")
+
+        for file_name in files:            
+            report_metrics(file_name, args.pred_col_title, args.target_col_title, nle_metrics)
+        
 
 if __name__ == "__main__":
     # stuff only to run when not called via 'import' here
